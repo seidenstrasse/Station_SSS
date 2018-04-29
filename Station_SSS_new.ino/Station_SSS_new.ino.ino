3d3b985ce5173b82139833679985edd53de6e170
TheOnlyRoxion <tobiasnixdorf@Tobias-MacBook-Pro.local>
2018-04-30 01:34:29 +0200

    jo

--- Station_SSS_new.ino/Station_SSS_new.ino.ino
+++ Station_SSS_new.ino/Station_SSS_new.ino.ino
@@ -1,6 +1,5 @@
 
 #include <sss7.h>
-
 #include <Arduino.h>
 #include "ardusss7.h"
 #include "Keypad.h"
@@ -8,10 +7,11 @@
 #include <LiquidCrystal.h>
 #include <EEPROM.h>
 
+//#define MODE_SENDING_INFORMATION 99
 
-//#define ID 2
 
 
+//Keypad Definitions
 const byte ROWS = 4; //four row
 const byte COLS = 4; //four columns
 char keys[ROWS][COLS] =
@@ -20,78 +20,81 @@ char keys[ROWS][COLS] =
   {'7', '8', '9', 'C'},
   {'*', '0', '#', 'D'}
 };
+//Define Row Pins
 byte rowPins[ROWS] = {
   33, 31, 29, 27
-}; //connect to the row pinouts of the keypad
+};
+//Define Col Pins
 byte colPins[COLS] = {
   32, 30, 28, 26
-}; //connect to the column pinouts of the keypad
-int count = 0;
+};
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-///////////////////////////////////////////////////////
+LiquidCrystal lcd(38, 36, 52, 50, 48, 46 );
 
-
-///////////////////////////////////////////////////////////
-
-
-
-
-unsigned long i;
+//Declaring Variables
 int blinking;
 
-char key = -1;
+uint8_t ID;
+uint8_t Page = 0;
+uint8_t Mode;
+
+char key="X" ;
 int nkey;
-char key1 = -1;
+char key1= "Y" ;
 int nkey1;
 int dest = -1;
-char type = 'Z';
-char confirmDest = -1;
-unsigned long saveNewMillis;
 
-int site = 0;
-int state = 0;
-unsigned long delaytime = 10;
-unsigned long delaytimesend = 2000;
-unsigned long delaytimereceive = 9000;
-unsigned long timenow;
-bool fail = false;
+int checkkey;
+
+char type = 'Z';
+
+unsigned long TimePlusDelayNextPage;
+unsigned long TimePlusDelaySending;
+unsigned long TimePlusDelayRecieving;
+
+
+unsigned long DelayInitDialog = 2000;
+unsigned long DelayTimeWhenSending = 80000;
+unsigned long DelayTimeWhenRecieving = 20000;
+
+bool SomeBoolVariable;
+bool SomeOtherBoolVariable;
+bool SomeFurtherBoolVariable;
+
 uint8_t msg[SSS7_PAYLOAD_SIZE];
 uint8_t msg1[SSS7_PAYLOAD_SIZE];
-char StationNumber = '5';
-uint8_t ID;
-bool stuff = false;
-bool thing = true;
-/////////////////////////////////////////////////////////////
+
 void setup()
 {
-  ID = EEPROM.read(1);
+  //Turn On LCD
   pinMode(22, OUTPUT);
   digitalWrite(22, HIGH);
 
 
+
+  //   ID = EEPROM.read(1);
+  //  pinMode(22, OUTPUT);
+  //  digitalWrite(22, HIGH);
+  ID = 2;
+
+  //LCD Start
   lcd.begin(16, 2);
   Serial.begin(9600);
   Serial.print("init");
+  //SSS7 Start
   SSS7.init();
-  // set up the LCD's number of columns and rows:
 
-
-  // Print a message to the LCD.
-
+  //Print Message
   lcd.setCursor(12, 0);
   lcd.print("ID=");
   lcd.setCursor(15, 0);
   lcd.print(ID);
-
-
-
   lcd.setCursor(0, 0);
 
   lcd.print("HELLO!");
 
-  delay(2000);
+  delay(1000);
   lcd.clear();
 
   lcd.setCursor(0, 0);
@@ -100,126 +103,103 @@ void setup()
 
 
 
-  delay(2000);
+  delay(1000);
 
 
 }
 
 
+void loop()
+{
+  
 
-
-
-
-/////////////////////////////////////////////////////////////
-
-  //   Serial.println("run");
-  switch (state) {
+  switch (Mode) {
     case 0:
-      timenow = millis()+delaytime;
+      //reset
+      TimePlusDelayNextPage = millis() + DelayInitDialog;
+      Mode = 1;
+      break;
 
 
-      //reset stuff
-      key = -1;
-      key1 = -1;
-      dest = -1;
-      state=1;
-break;
-    case 1:  // ASK FOR SEND ALTERNATIVES
+    case 1:
+      //Display Text stuff
 
-      Serial.println(site);
-      //Anzeige Text
-      if (site == 0) {
+      if (Page == 0) {
         lcd.setCursor(0, 0);
         lcd.print("HOW to SEND         ");
         lcd.setCursor(0, 1);
         lcd.print("PRESS:           ");
       }
 
-      if (site == 1) {
-        //lcd.clear();
+      if (Page == 1) {
         lcd.setCursor(0, 0);
         lcd.print("A FOR DATAMATRIX    ");
         lcd.setCursor(0, 1);
         lcd.print("B FOR ADDRESS       ");
       }
-      if (site == 2) {
+
+      if (Page == 2) {
         lcd.setCursor(0, 0);
         lcd.print("FOR HELP VISIT        ");
         lcd.setCursor(0, 1);
         lcd.print("silkroad.x-ra.de    ");
       }
 
+
       type = keypad.getKey();
 
-
       if (type == 'A') {
-        state = 6;
-        site = 0;
+        Mode = 6;
+        Page = 0;
       }
 
       if (type == 'B') {
-        state = 2;
-        site = 0;
+        Mode = 2;
+        Page = 0;
       }
 
-
-      //
-      //                              Serial.println("blubb");
-      //                              Serial.println(millis());
-      //      Serial.println(timenow);
-
-
-   
-
-
-
-      if (millis() > timenow) {
-        site++;
-
-   Serial.print(millis());
-      Serial.print(" > ");
-      Serial.print(timenow);
-      
-Serial.println(site);
-if (site >= 3) {
-          site = 0;
-          state = 0;
-       }
-
+      if (millis() > TimePlusDelayNextPage) {
+        Page++;
+        lcd.clear();
+        TimePlusDelayNextPage = millis() + DelayInitDialog;
       }
 
-
+      if (Page == 3) {
+        Page = 0;
+      }
       break;
 
     case 2: //INSERT DESTINATION
       lcd.clear();
-      lcd.print("DESTINATION: ##   ");
-      lcd.setCursor(13, 0);
-      key = keypad.getKey();
-      if (key != '#' || key != '*' || key != 'A' || key != 'B' || key != 'C' || key != 'D') {
+      lcd.print("DESTINATION:_##____     ");
+
+
+      key = keypad.waitForKey();
+    
+      
         lcd.setCursor(13, 0);
         lcd.print(key);
-        state = 3;
-      }
-      break;
+          lcd.setCursor(15, 0);
+    lcd.print("_");
+        Mode = 3;
 
+      break;
 
 
     case 3: //INSERT DESTINATION
-      lcd.print("DESTINATION:  #   ");
-      key1 = keypad.getKey();
-      if (key1 != '#' || key1 != '*' || key1 != 'A' || key1 != 'B' || key1 != 'C' || key1 != 'D') {
+      lcd.print("DESTINATION:_ #___      ");
+      key1 = keypad.waitForKey();
+      
         lcd.setCursor(14, 0);
         lcd.print(key1);
-        state = 4;
+     
+        Mode = 4;
         nkey = (int)key;
         nkey1 = (int)key1;
-        dest = (nkey - 48) * 10 + (nkey1 - 48);
-      }
+        //dest = (nkey - 48) * 10 + (nkey1 - 48);
+           dest = (nkey - '0') * 10 + (nkey1 -'0');   
+      //Serial.println(dest);
       break;
-
-
-
 
     case 4:
       lcd.setCursor(0, 1);
@@ -228,12 +208,10 @@ if (site >= 3) {
       if (keypad.getKey() == '#') {
         lcd.clear();
         lcd.print((int)dest);
-        state = 7;
+        Mode = 7;
       }
       break;
 
-
-    //////////////////////////////////////////////////////////////////////////////
     case 6: //DATAMATRIX SEND DIALOG
       lcd.clear();
 
@@ -243,12 +221,10 @@ if (site >= 3) {
 
       if (keypad.getKey() == '#') {
         lcd.clear();
-        state = 7;
+        Mode = 7;
         dest = -1;
       }
       break;
-
-
 
     case 7:
       lcd.clear();
@@ -262,12 +238,10 @@ if (site >= 3) {
         digitalWrite(22, HIGH);
         delay(200);
       }
-      state = 99;
+      Mode = 99;
       break;
 
-
-
-
+    //Error case
     case 42:
       lcd.clear();
       lcd.setCursor(0, 0);
@@ -281,17 +255,14 @@ if (site >= 3) {
         digitalWrite(13, HIGH);
         delay(100);
       }
-      state = 0;
+      Mode = 0;
       break;
 
 
-
-
-
     case 99://///////////////////////////////////////////////////////////
-      timenow = millis() + delaytimesend;
+      TimePlusDelaySending = millis() + DelayTimeWhenSending;
 
-      stuff = false;
+      SomeBoolVariable = false;
 
       if (dest == -1) {
         msg[0] = MSG_REQUEST_BARCODE;
@@ -305,18 +276,19 @@ if (site >= 3) {
         SSS7.send(msg);
 
         while (!SSS7.canSend());
-        stuff = SSS7.sendFailed();
+        SomeBoolVariable = SSS7.sendFailed();
         Serial.println("Finished Sending");
+        Serial.println(SomeBoolVariable);
         // break;
 
-        if (stuff) {
+        if (SomeBoolVariable==true) {
           Serial.println("Send failed");
-          state = 42;
+          Mode = 42;
         }
 
-        //42
+
       } else {
-        stuff = false;
+        SomeBoolVariable = false;
         msg[0] = MSG_REQUEST_DIRECT;
         msg[1] = ID;
         msg[2] = ID_COORDINATOR;
@@ -328,49 +300,44 @@ if (site >= 3) {
         SSS7.send(msg);
 
         while (!SSS7.canSend());
-        stuff = SSS7.sendFailed();
+        SomeBoolVariable = SSS7.sendFailed();
+        Serial.println(SomeBoolVariable);
         Serial.println("Finished Sending");
         // break;
 
 
-        if (stuff) {
+        if (SomeBoolVariable) {
           Serial.println("Send failed");
-          state = 42;
+          Mode = 42;
 
         }
 
 
       }
 
-      if (millis() > timenow && stuff) {
-        state = 42;
-      }
-      if (millis() < timenow && stuff) {
-        state = 99;
-      }
-      if (stuff == false) {
-        state = 100;
+      if (millis() > TimePlusDelaySending && SomeBoolVariable) {
+        Mode = 42;
+      } else if (millis() <= TimePlusDelaySending && SomeBoolVariable) {
+        Mode = 99;
+      } else if (SomeBoolVariable == false) {
+        Mode = 100;
       }  ;
 
-
-
-
       break;
-
-
-
     case 100://////////////////////////////////////////////////////////////
-      timenow = millis() + delaytimereceive;
-      thing = true;
+    Serial.println("Zustand 100");
+      TimePlusDelayRecieving = millis() + DelayTimeWhenRecieving;
+      SomeOtherBoolVariable = true;
       delay(200);
       if (SSS7.hasReceived()) {
+        
         uint8_t msg1[SSS7_PAYLOAD_SIZE];
         SSS7.getReceived(msg1);
 
         Serial.print("Got data:");
         Serial.println((char*)msg1);
         if ((msg1[0] == MSG_CONFIRM_REQUEST) && (msg1[2] == ID)) {
-          thing = false;
+          SomeOtherBoolVariable = false;
           Serial.println(msg1[0]);
           Serial.println(msg1[1]);
           Serial.println(msg1[2]);
@@ -378,31 +345,25 @@ if (site >= 3) {
 
 
           lcd.setCursor(0, 0);
-          lcd.print("WORKING FOR YOU        ");
+          lcd.print("FOMPWAT     ");
           lcd.setCursor(0, 1);
-          lcd.print("PLEASE WAIT            ");
+          lcd.print("WAITWAT            ");
           lcd.setCursor(11, 1);
           lcd.print(msg1[3]);
 
-          state = 101;
-          timenow = millis() + delaytimereceive;
+          Mode = 101;
+
 
           break;
         }
       }
-
-
-
-
-      if (millis() < timenow && thing ) {
-        state = 42; /////Error timeout
+      if (millis() > TimePlusDelayRecieving && SomeOtherBoolVariable ) {
+        Mode = 42; /////Error timeout
       }
-
-
       break;
     case 101:
-
-      thing = true;
+Serial.println("Zustand 101");
+      SomeFurtherBoolVariable = true;
       if (SSS7.hasReceived()) {
 
         SSS7.getReceived(msg1);
@@ -411,7 +372,7 @@ if (site >= 3) {
         Serial.println((char*)msg1);
 
         if ((msg1[0] == MSG_BEGIN_TRANSFER) && (msg1[2] == ID)) {
-          thing = false;
+          SomeFurtherBoolVariable = false;
           Serial.println(msg1[0]);
           Serial.println(msg1[1]);
           Serial.println(msg1[2]);
@@ -421,7 +382,7 @@ if (site >= 3) {
           lcd.setCursor(0, 0);
           lcd.print("INSERT CAPSULE !!!             ");
           lcd.setCursor(0, 1);
-          lcd.print(" WE WORK FOR YOU                       ");
+          lcd.print(" FOMPINGWAT                       ");
           while (blinking < 20) {
             blinking++;
             digitalWrite(22, LOW);
@@ -435,15 +396,18 @@ if (site >= 3) {
         lcd.setCursor(0, 1);
         lcd.print("SEIDENSTRASSE           ");
 
-        state = 0;
+        Mode = 0;
         break;
       }
-      if (millis() < timenow) {
-        state = 42; /////Error timeout
+      if (millis() > TimePlusDelayRecieving) {
+        Mode = 42; /////Error timeout
       }
 
   }
 }
+
+
+
 
 
 
